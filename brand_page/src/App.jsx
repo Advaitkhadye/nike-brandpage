73e46645c548327dd2470081cdd1a0c56480283f
Advait <advaitkhadye08@gmail.com>
2023-08-25 16:15:06 +0530

    first commit

---
+++ brand_page/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./assets/components/Hero";
+import Navigation from "./assets/components/Navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
